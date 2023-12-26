922b5b50d73ad92e2f266f13b100bc197acc18cc
nishad0011 <nishadgund@gmail.com>
2023-12-26 14:11:38 +0530

    commit No 2

--- DSA Course/ArrayADT.cpp
+++ DSA Course/ArrayADT.cpp
@@ -8,6 +8,13 @@ struct Array
     int current_length;
 };
 
+void Swap(int *a, int *b)
+{
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
 void Display(struct Array *Arr)
 {
     for (int i = 0; i < Arr->current_length; i++)
@@ -50,7 +57,46 @@ void Insert(struct Array *p, int index, int value)
     // Display modified array
     cout << "Array after insertion :" << endl;
     Display(p);
+}
+
+void Delete(struct Array *p, int index)
+{
+    if (index < 1 || index > p->current_length)
+        cout << "Unable to delete.." << endl;
+    else
+    {
+        for (int x = index; x < p->current_length; x++)
+        {
+            *((p->A) + x - 1) = *((p->A) + x);
+        }
+        *(p->A + (p->current_length) - 1) = 0;
+        p->current_length--;
+        // Display modified array
+        cout << "Array after Deletion :" << endl;
+        Display(p);
+    }
 };
+
+void LinearSearch(struct Array *p, int element)
+{
+    bool el_found = false;
+    for (int i = 0; i < p->current_length; i++)
+    {
+        if (*(p->A + i) == element)
+        {
+            cout << "Element found at index " << i + 1 << endl;
+            el_found = true;
+            if (i != 0)
+                Swap(p->A + i, p->A + i - 1);
+        }
+    }
+    if (!el_found)
+        cout << "Element not found" << endl;
+
+    // Display modified array
+    cout << "Array after Search :" << endl;
+    Display(p);
+}
 
 int main()
 {
@@ -58,7 +104,7 @@ int main()
     int tempArrLen = sizeof(tempArr) / sizeof(int);
     int Array[20] = {}; //{} initializes array of 0s.
 
-    // initializing array Array[10]
+    // initializing array Array[10] with tempArr[]
     for (int i = 0; i < tempArrLen; i++)
         Array[i] = tempArr[i];
 
@@ -72,6 +118,7 @@ int main()
     Display(&Arr);
     Append(&Arr, 999);
     Insert(&Arr, 3, 1000);
-
+    // Delete(&Arr, 1);
+    LinearSearch(&Arr, 999);
     return 0;
 }
---
+++ DSA Course/BinarySearch.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+};
+
+void Recursive_Binary_Search(int A, int element)
+{
+    int min =
+}
+
+int main()
+{
+    int Array[] = {2, 4, 6, 8, 10, 12};
+    struct Array Arr;
+    Arr.A = Array;
+    Arr.size = sizeof(Array) / sizeof(int);
+
+    Recursive_Binary_Search(Array, 6);
+
+    return 0;
+}
